6253494b1fee5479949d336f65427157fa7d1989
Paolo Dávila <pdavila@iesebre.com>
2015-04-08 20:33:16 +0200

    Working example Stupid Solid OK

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -31,10 +31,17 @@
 				<a class="navbar-brand" href="#">Laravel</a>
 			</div>
 
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="{{ url('/') }}">Home</a></li>
+                    <li><a href="{{ url('/invoice') }}">Invoice</a></li>
+                </ul>
+            <!--
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
 					<li><a href="{{ url('/') }}">Home</a></li>
 				</ul>
+				-->
 
 				<ul class="nav navbar-nav navbar-right">
 					@if (Auth::guest())
